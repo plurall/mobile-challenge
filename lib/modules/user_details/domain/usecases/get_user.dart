9c4b112ea1b276868224f66064871c8a6f81880d
Max <raranmariano@hotmail.com>
2021-05-22 19:05:02 -0300

    Add single get User usecase for user_details

---
+++ lib/modules/user_details/domain/usecases/get_user.dart
@@ -0,0 +1,23 @@
+import 'package:mobile_challenge/clean/usecase.dart';
+import 'package:mobile_challenge/modules/user_details/data/repositories/github_user_api_repository.dart';
+import 'package:mobile_challenge/shared/entities/User.dart';
+
+class GetUser implements UseCase<User, GetUserParams> {
+  GetUser(this.githubRepo);
+
+  final GithubUserApiRepositoryProtocol githubRepo;
+
+  @override
+  Future<User> call(GetUserParams params) async {
+    return await githubRepo.getUser(params.nickname);
+  }
+}
+
+class GetUserParams {
+  GetUserParams(this.nickname);
+
+  final String nickname;
+
+  @override
+  List<Object> get props => [];
+}
