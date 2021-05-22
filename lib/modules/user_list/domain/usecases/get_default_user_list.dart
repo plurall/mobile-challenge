f8fe704c4bd0f66371d3762e279f325f2d1eb8c1
Max <raranmariano@hotmail.com>
2021-05-22 04:03:57 -0300

    Add usecases for presentation

---
+++ lib/modules/user_list/domain/usecases/get_default_user_list.dart
@@ -0,0 +1,14 @@
+import 'package:mobile_challenge/clean/usecase.dart';
+import 'package:mobile_challenge/modules/user_list/data/repositories/github_api_repository.dart';
+import 'package:mobile_challenge/modules/user_list/domain/entities/User.dart';
+
+class GetDefaultUserList implements UseCase<List<User>, NoParams> {
+  GetDefaultUserList(this.githubRepo);
+
+  final GithubApiRepositoryProtocol githubRepo;
+
+  @override
+  Future<List<User>> call(NoParams params) async {
+    return await githubRepo.getDefaultUserList();
+  }
+}
---
+++ lib/modules/user_list/domain/usecases/get_user_search.dart
@@ -0,0 +1,23 @@
+import 'package:mobile_challenge/clean/usecase.dart';
+import 'package:mobile_challenge/modules/user_list/data/repositories/github_api_repository.dart';
+import 'package:mobile_challenge/modules/user_list/domain/entities/User.dart';
+
+class GetUserSearch implements UseCase<List<User>, GetUserSearchParams> {
+  GetUserSearch(this.githubRepo);
+
+  final GithubApiRepositoryProtocol githubRepo;
+
+  @override
+  Future<List<User>> call(GetUserSearchParams params) async {
+    return await githubRepo.getUserSearch(params.query);
+  }
+}
+
+class GetUserSearchParams {
+  GetUserSearchParams(this.query);
+
+  final String query;
+
+  @override
+  List<Object> get props => [];
+}
