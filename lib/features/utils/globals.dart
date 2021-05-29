1a630b036a54038f1045bab0fd594ed2160c1b36
weldon.souza <weldonrafael@gmail.com>
2021-05-29 14:36:35 -0300

    criação das constantes globais

---
+++ lib/features/utils/globals.dart
@@ -0,0 +1,9 @@
+library my_prj.globals;
+
+//******************  CONSTANTES GLOBAIS  **********************
+const String TOKEN = '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';
+const String DOMAIN = 'https://api.github.com';
+const String PATH_PROFILE_USER = '/users/';
+const String userAlreadyExist = 'Usuário já consta na lista';
+const String userNotFound = 'Usuário não encontrado';
+const String noValue = 'Não informado';
