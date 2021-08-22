242b30dae622afaf70ae80d5df6863a7bea06e10
Joao Armando <joaoaarmando@hotmail.com>
2021-08-21 22:30:05 -0300

    Should convert a UserDetailModel in a UserDetailEntity with the correct values

--- test/app/modules/user/features/profile/infra/models/user_detail_model_test.dart
+++ test/app/modules/user/features/profile/infra/models/user_detail_model_test.dart
@@ -6,6 +6,15 @@ import 'package:mobile_challenge/app/modules/user/features/profile/infra/models/
 import '../../../../../fixtures/fixture_reader.dart';
 
 void main() {
+  final tUser = UserDetailModel(
+    login: "joaoarmando", 
+    bio: "Olá, Plurall!", 
+    name: "João Armando", 
+    location: "Petrópolis - RJ", 
+    email: "joaoaarmando@hotmail.com", 
+    avatarUrl: "https://randopictureurl.com"
+  );
+
   test('Should convert a json to a UserDetailModel with the correct values', () async {
     final json = jsonDecode(fixture("user_detail_response.json"));
 
@@ -20,15 +29,6 @@ void main() {
   });
 
   test('Should convert a UserDetailModel to a json with the correct values', () async {
-    final tUser = UserDetailModel(
-      login: "joaoarmando", 
-      bio: "Olá, Plurall!", 
-      name: "João Armando", 
-      location: "Petrópolis - RJ", 
-      email: "joaoaarmando@hotmail.com", 
-      avatarUrl: "https://randopictureurl.com"
-    );
-
     final result = tUser.toMap();
 
     expect(result["login"], tUser.login);
@@ -37,5 +37,16 @@ void main() {
     expect(result["location"], tUser.location);
     expect(result["email"], tUser.email);
     expect(result["avatar_url"], tUser.avatarUrl);
+  });
+
+  test('Should convert a UserDetailModel in a UserDetailEntity with the correct values', () async {
+    final result = tUser.toEntity();
+
+    expect(result.login, tUser.login);
+    expect(result.avatarUrl,tUser.avatarUrl);
+    expect(result.name, tUser.name);
+    expect(result.bio, tUser.bio);
+    expect(result.email, tUser.email);
+    expect(result.location, tUser.location);
   });
 }
