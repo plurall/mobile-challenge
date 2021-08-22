7236c62ea0dfb6e863f870f9745245eef0b0a077
Joao Armando <joaoaarmando@hotmail.com>
2021-08-21 22:01:13 -0300

    Should convert a UserDetailModel to a json with the correct values

--- test/app/modules/user/features/profile/infra/models/user_detail_model_test.dart
+++ test/app/modules/user/features/profile/infra/models/user_detail_model_test.dart
@@ -15,6 +15,27 @@ void main() {
     expect(result.avatarUrl, json["avatar_url"]);
     expect(result.name, json["name"]);
     expect(result.bio, json["bio"]);
+    expect(result.email, json["email"]);
     expect(result.location, json["location"]);
+  });
+
+  test('Should convert a UserDetailModel to a json with the correct values', () async {
+    final tUser = UserDetailModel(
+      login: "joaoarmando", 
+      bio: "Olá, Plurall!", 
+      name: "João Armando", 
+      location: "Petrópolis - RJ", 
+      email: "joaoaarmando@hotmail.com", 
+      avatarUrl: "https://randopictureurl.com"
+    );
+
+    final result = tUser.toMap();
+
+    expect(result["login"], tUser.login);
+    expect(result["bio"], tUser.bio);
+    expect(result["name"], tUser.name);
+    expect(result["location"], tUser.location);
+    expect(result["email"], tUser.email);
+    expect(result["avatar_url"], tUser.avatarUrl);
   });
 }
