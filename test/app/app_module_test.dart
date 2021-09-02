a98bf23aff090306dc11e892799ead16a9435051
Fabrício <fajusto@icloud.com>
2021-09-02 17:24:04 -0300

    - removed unused imports

--- test/app/app_module_test.dart
+++ test/app/app_module_test.dart
@@ -6,9 +6,7 @@ import 'package:flutter_modular/flutter_modular.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mobile_challenge/app/app_module.dart';
 import 'package:mobile_challenge/app/app_widget.dart';
-import 'package:mobile_challenge/app/modules/search/domain/entities/result_search.dart';
 import 'package:mobile_challenge/app/modules/search/domain/usecases/search_by_text.dart';
-import 'package:mobile_challenge/app/modules/search/utils/github_response.dart';
 import 'package:mockito/mockito.dart';
 
 class DioMock extends Mock implements Dio {}
